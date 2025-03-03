1d93fd3a94bce3b0284327e47eccfd5eae9d9cc5
Hugo Rodia <hugorodia@gmail.com>
2025-03-03 10:42:31 -0300

    Versión básica de functions.php para resolver 503

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 use Pusher\Pusher;
 
 header('Content-Type: application/json');
-error_log("Iniciando functions.php - Versión mínima para prueba");
+error_log("Iniciando functions.php - Versión básica para prueba 503");
 
 echo json_encode(['success' => true, 'message' => 'Servidor funcionando sin base de datos']);
 exit;
